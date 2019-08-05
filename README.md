6b4b2d50161468236e084a3321aec1dda537f759
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-08-05 13:42:10 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,60 @@
+# Dashboard multi-direction
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+### Description 
+
+This example shows series/axes progressing to all kinds of directions inside a dashboard.
+
+### API links
+
+* Dashboard: [Dashboard][]
+* Solid FillStyle: [SolidFill][]
+* RGBA color factory: [ColorRGBA][]
+* XY cartesian chart: [ChartXY][]
+* Axis: [Axis][]
+* Scroll strategies: [AxisScrollStrategies][]
+* Freeform line series: [LineSeries][]
+* Default library style: [DefaultLibraryStyle][]
+
+
+### Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+© Arction Ltd 2009-2019. All rights reserved.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/axis.html
+[AxisScrollStrategies]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#axisscrollstrategies
+[ChartXY]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/chartxy.html
+[ColorRGBA]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#colorrgba
+[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/dashboard.html
+[DefaultLibraryStyle]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#defaultlibrarystyle
+[LineSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/lineseries.html
+[SolidFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidfill.html
---
+++ src/index.js
@@ -0,0 +1,103 @@
+/**
+ * LightningChartJS example that showcases series/axes progressing to all kinds of directions inside a dashboard.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    SolidFill,
+    ColorRGBA,
+    AxisScrollStrategies,
+    DataPatterns
+} = lcjs
+
+// Import data-generators from 'xydata'-library.
+const {
+    createProgressiveTraceGenerator,
+    createTraceGenerator
+} = require('@arction/xydata')
+
+// Create a 3x3 dashboard.
+const grid = lightningChart().Dashboard({
+    numberOfRows: 3,
+    numberOfColumns: 3
+})
+    .setBackgroundFillStyle(new SolidFill().setColor(ColorRGBA(24, 24, 24)))
+
+// Add charts to dashboard.
+const cells = [
+    { row: 1, col: 0 },
+    { row: 2, col: 1 },
+    { row: 1, col: 2 },
+    { row: 0, col: 1 },
+    { row: 1, col: 1 }
+]
+const chooseRandom = (options) => options[Math.round(Math.random() * (options.length - 1))]
+const createCell = (cell) => {
+    const chart = grid.createChartXY({
+        columnIndex: cell.col,
+        rowIndex: cell.row,
+        columnSpan: 1,
+        rowSpan: 1
+    })
+    // Add a random omni-directional series.
+    const type = chooseRandom(['PointSeries', 'LineSeries'])
+    // Setup data-generation for series.
+    if (cell.row == cell.col) {
+        const series = chart['add' + type]()
+        // Random trace
+        createTraceGenerator()
+            .setNumberOfPoints(100000)
+            .generate()
+            .setStreamInterval(50)
+            .setStreamBatchSize(10)
+            .setStreamRepeat(true)
+            .toStream()
+            .forEach(point => series.add(point))
+    } else {
+        // Random progressive trace with mapped direction.
+        const flipPlane = cell.col == 1
+        const mul = { x: cell.col == 0 ? -1 : 1, y: cell.row == 0 ? 1 : -1 }
+        // Configure axes.
+        let axisX = chart.getDefaultAxisX(), axisY = chart.getDefaultAxisY()
+        if (cell.row == cells.reduce((prev, cell) => Math.max(prev, cell.row), 0)) {
+            axisX.dispose()
+            axisX = chart.addAxisX(true)
+        }
+        if (cell.col == 0) {
+            axisY.dispose()
+            axisY = chart.addAxisY(true)
+        }
+        if (mul.x < 0) {
+            axisX
+                .setInterval(-100, 0)
+                .setScrollStrategy(flipPlane ? AxisScrollStrategies.fitting : AxisScrollStrategies.regressive)
+        } else
+            axisX
+                .setInterval(0, 100)
+                .setScrollStrategy(flipPlane ? AxisScrollStrategies.fitting : AxisScrollStrategies.progressive)
+
+        if (mul.y < 0) {
+            axisY
+                .setInterval(-100, 0)
+                .setScrollStrategy(flipPlane ? AxisScrollStrategies.regressive : AxisScrollStrategies.fitting)
+        } else
+            axisY
+                .setInterval(0, 100)
+                .setScrollStrategy(flipPlane ? AxisScrollStrategies.progressive : AxisScrollStrategies.fitting)
+
+        const series = chart['add' + type](axisX, axisY)
+        createProgressiveTraceGenerator()
+            .setNumberOfPoints(100000)
+            .generate()
+            .setStreamInterval(50)
+            .setStreamBatchSize(2)
+            .setStreamRepeat(true)
+            .toStream()
+            .forEach(point => series.add({ x: (flipPlane ? point.y : point.x) * mul.x, y: (flipPlane ? point.x : point.y) * mul.y }))
+    }
+    return chart.setTitle(type)
+}
+cells.map(createCell)
