8dd6aecf6638cde5ca017efa99b375ac4d8dc6e4
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:11:14 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ const { createProgressiveTraceGenerator, createTraceGenerator } = xydata
 
 // Create a 3x3 dashboard.
 // NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/basic-topics/grouping-charts/
-const grid = lightningChart().Dashboard({
+const grid = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        }).Dashboard({
     theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     numberOfRows: 3,
     numberOfColumns: 3,
